6e1eb0e88eac013b03ceeba70f545f3234b27da2
Joel Wiesner <j.wiesner88@gmail.com>
2023-03-09 13:01:44 -0800

    Using data.js - added menu info/items to HTML

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 import { menuArray } from "./data.js"
 
 
-menuArray.forEach(menuItem => {
-  console.log (menuItem.name)
-})
+
+// menuArray.forEach(function(menuOptions) { 
+//     const itemName = document.getElementById('item-name')
+//     console.log(menuOptions.name)
+// })
+
+function displayMenu() {
+
+    let menuInfo = ``
+
+    menuArray.forEach(function(menuItem) {
+        menuInfo += `
+        <div id="menu-pic">${menuItem.emoji}</div>
+        <div id="menu-info">
+            <div id="item-name">${menuItem.name}</div>
+            <div id="item-ingredients">${menuItem.ingredients}</div>
+            <div id="item-price">${menuItem.price}</div>
+        </div>
+        `
+    })
+    return menuInfo
+}
+
+function renderMenu(){
+    document.getElementById('menu-items').innerHTML = displayMenu()
+}
+
+renderMenu()
+
