14ede186322d98db610028124f0e1d5c754ca6d9
Joel Wiesner <j.wiesner88@gmail.com>
2023-03-20 19:16:06 -0700

    working on eventListeners

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 import { menuArray } from "./data.js"
 
 
-function renderMenu(){
-    document.getElementById('menu-items').innerHTML = displayMenu()
-}
-
-renderMenu()
-
 
 // Loop through data.js and display menu item in HTML //
 
@@ -14,7 +8,11 @@ function displayMenu() {
 
     let menuInfo = ``
 
+    let addItemBtn = document.querySelector(`[data-add-item-id="${menuItem.id}"]`)
+        addItemBtn.addEventListener("click", displayOrder)
+
     menuArray.forEach(function(menuItem) {
+
         menuInfo += `
         <div class="menu-wrapper">
             <div class="menu-pic" id="menu-pic">${menuItem.emoji}</div>
@@ -23,12 +21,71 @@ function displayMenu() {
                 <div  class="item-ingredients" id="item-ingredients">${menuItem.ingredients}</div>
                 <div class="item-price" id="item-price">$${menuItem.price}</div>
             </div>
+            <button class="add-item-btn" data-add-item-id="${menuItem.id}">+</button>
         </div>
         <hr>
-        `
+        `        
+        
     })
+
+    
     return menuInfo
 }
+
+function renderMenu(){
+    document.getElementById('menu-items').innerHTML = displayMenu()
+}
+
+renderMenu()
+
+// Start working on code HERE!!
+const addItemBtn = document.querySelector(`[data-add-item-id="${menuItem.id}"]`)
+addItemBtn.addEventListener("click", displayOrder)
+
+
+
+function displayOrder() { 
+    
+    const orderDetails = document.getElementById('order-details')
+    const orderPizza = 0
+    const orderHamburger = 1
+    const orderBeer = 2
+
+    menuArray.forEach(function(item) {
+            if(item.id === orderPizza) 
+                orderDetails.innerHTML = `
+                <div class="order-wrapper">
+                    <div class="item-name" id="item-name">${item.name}</div>
+                    <div class="remove">remove</div>
+                    <div class="item-price" id="item-price">$${item.price}</div>
+                </div
+                `
+             else if(item.id === orderHamburger) 
+                orderDetails.innerHTML = `
+                <div class="order-wrapper">
+                    <div class="item-name" id="item-name">${item.name}</div>
+                    <div class="remove">remove</div>
+                    <div class="item-price" id="item-price">$${item.price}</div>
+                </div>
+                `
+             else (item.id === orderBeer) 
+                orderDetails.innerHTML = `
+                <div class="order-wrapper">
+                    <div class="item-name" id="item-name">${item.name}</div>
+                    <div class="remove">remove</div>
+                    <div class="item-price" id="item-price">$${item.price}</div>
+                </div>
+              `
+        })
+}    
+
+// function renderOrder() {
+//     document.getElementById('order-details').innerHTML = displayOrder()
+// }
+
+// renderOrder()
+
+
 
 
 // Add menu button //
